c83fb16376d3f84373b54fc272a39289ce8e4c86
mozhe007 <mozhe007@qq.com>
2021-02-21 18:01:42 +0800

    初始化项目

---
+++ src/main/java/heyue/listen_led/ListenLedApplication.java
@@ -0,0 +1,13 @@
+package heyue.listen_led;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenLedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenLedApplication.class, args);
+    }
+
+}
